e7e8bba9e3ef55c0ead0b22dcddc25784e87106d
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-03-24 19:15:09 +0700

    # Init. Before rows css styles

--- sources/models/records.js
+++ sources/models/records.js
@@ -1,10 +1,25 @@
 export const data = new webix.DataCollection({
   data: [
-    {id: 1, title: "The Shawshank Redemption", year: 1994, votes: 678790, rating: 9.2, rank: 1},
-    {id: 2, title: "The Godfather", year: 1972, votes: 511495, rating: 9.2, rank: 2},
-    {id: 3, title: "The Godfather: Part II", year: 1974, votes: 319352, rating: 9.0, rank: 3},
-    {id: 4, title: "The Good, the Bad and the Ugly", year: 1966, votes: 213030, rating: 8.9, rank: 4},
-    {id: 5, title: "My Fair Lady", year: 1964, votes: 533848, rating: 8.9, rank: 5},
-    {id: 6, title: "12 Angry Men", year: 1957, votes: 164558, rating: 8.9, rank: 6}
+    {NUM: 1, FULLNAME: "Балахнина Екатерина Сергеевна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 2, FULLNAME: "Болдузев Денис Вячеславович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 3, FULLNAME: "Векессер Павел Владимирович", IS_ACTIVE: 0, SKIPPED: 1, LAST_ACTIVE: null,},
+    {NUM: 4, FULLNAME: "Венерцева Наталья Викторовна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null,},
+    {NUM: 5, FULLNAME: "Даминов Владислав Валитович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 6, FULLNAME: "Иванов Павел Нугзарович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 7, FULLNAME: "Кулиш Виктор Владимирович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 8, FULLNAME: "Кебереков Игорь Павлович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 9, FULLNAME: "Манышев Руслан Григорьевич", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 10, FULLNAME: "Крюкова Елена Александровна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 11, FULLNAME: "Курносова Ирина Ивановна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 12, FULLNAME: "Лесников Максим Александрович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 13, FULLNAME: "Молодых Кристина Валерьевна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 14, FULLNAME: "Старовойтов Алексей Борисович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 15, FULLNAME: "Тайлунов Алан Артурович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 16, FULLNAME: "Трашева Ирина Александровна", IS_ACTIVE: 0, SKIPPED: 1, LAST_ACTIVE: null},
+    {NUM: 17, FULLNAME: "Трашева Ирина Юрьевна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 18, FULLNAME: "Тибереков Леонид Евгеньевич", IS_ACTIVE: 0, SKIPPED: 1, LAST_ACTIVE: null},
+    {NUM: 19, FULLNAME: "Чиндашев Юрий Иосифович", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 20, FULLNAME: "Шипкова Кристина Александровна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
+    {NUM: 21, FULLNAME: "Челтушева Регина Артуровна", IS_ACTIVE: 0, SKIPPED: null, LAST_ACTIVE: null},
   ]
 });
--- sources/views/data.js
+++ sources/views/data.js
@@ -5,9 +5,25 @@ export default class DataView extends JetView {
   config() {
     return {
       view: "datatable",
-      autoConfig: true,
-      autoWidth: true,
-      css: "webix_shadow_medium"
+      // data,
+      // autoConfig: true,
+      columns: [
+        {id: "FULLNAME", header: "Наименование", adjust: "data"},
+        {id: "IS_ACTIVE", header: "Дежурный", adjust: "header", css:{'text-align':'center'}},
+        {id: "SKIPPED", header: "Освобождение от дежурства _", adjust: "header", css:{'text-align':'center'}},
+        {id: "LAST_ACTIVE", header: "Последнее дежурство", adjust: "header"},
+      ],
+      footer: true,
+      autowidth: true,
+      autoheight: true,
+      css: "webix_shadow_medium",
+      scheme:{
+        $change:function(item) {
+          console.log(item);
+          if (item.SKIPPED)
+            item.$css = "highlight";
+        }
+      }
     };
   }
   
