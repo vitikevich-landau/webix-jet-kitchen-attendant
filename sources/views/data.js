439618e6b09304fbc2dc9b92477e2d4267c93159
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-03-24 15:20:42 +0700

    # Init

---
+++ sources/views/data.js
@@ -0,0 +1,17 @@
+import {JetView} from "webix-jet";
+import {data} from "models/records";
+
+export default class DataView extends JetView {
+  config() {
+    return {
+      view: "datatable",
+      autoConfig: true,
+      autoWidth: true,
+      css: "webix_shadow_medium"
+    };
+  }
+  
+  init(view) {
+    view.parse(data);
+  }
+}
---
+++ sources/views/top.js
@@ -0,0 +1,45 @@
+import {JetView, plugins} from "webix-jet";
+
+
+export default class TopView extends JetView {
+  config() {
+    var header = {
+      type: "header", template: this.app.config.name, css: "webix_header app_header"
+    };
+    
+    var menu = {
+      view: "menu", id: "top:menu",
+      css: "app_menu",
+      width: 180, layout: "y", select: true,
+      template: "<span class='webix_icon #icon#'></span> #value# ",
+      data: [
+        {value: "Dashboard", id: "start", icon: "wxi-columns"},
+        {value: "Data", id: "data", icon: "wxi-pencil"}
+      ]
+    };
+    
+    var ui = {
+      
+      type: "space",
+      css: "app_layout",
+      cols: [
+        {
+          rows: [
+            {css: "webix_shadow_medium", rows: [header, menu]}
+          ]
+        },
+        {
+          rows: [
+            {$subview: true}
+          ]
+        }
+      ]
+    };
+    
+    return ui;
+  }
+  
+  init() {
+    this.use(plugins.Menu, "top:menu");
+  }
+}
